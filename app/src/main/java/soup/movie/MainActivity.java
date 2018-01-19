83be7c32bfe964e7c21e2e56ee749ca300652cab
Sungyong An <sungyong.an@naverlabs.com>
2018-01-20 01:36:42 +0900

    Splash: apply static splash screen

--- app/src/main/java/soup/movie/MainActivity.java
+++ app/src/main/java/soup/movie/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_Map);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
