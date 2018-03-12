c792fe52361cfa5d94232ace4a961918b50c7ea7
SOUP <soupyong@gmail.com>
2018-03-12 21:16:03 +0900

    UI: apply filter container using bottom sheet

--- app/src/main/java/soup/movie/ui/main/MainActivity.java
+++ app/src/main/java/soup/movie/ui/main/MainActivity.java
@@ -3,20 +3,25 @@ package soup.movie.ui.main;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import soup.movie.R;
 import soup.movie.ui.archive.ArchiveFragment;
 import soup.movie.ui.boxoffice.BoxOfficeFragment;
 import soup.movie.ui.home.HomeFragment;
-import soup.movie.ui.preview.MoviePreviewFragment;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
     private MainContract.Presenter mPresenter;
+
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +32,27 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         mPresenter = new MainPresenter();
         mPresenter.attach(this);
 
-        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
-        navigationView.setOnNavigationItemSelectedListener(item -> {
-            Timber.d("Select %s", item.getTitle());
-            setTitle(item.getTitle());
-            mPresenter.setTabMode(parseToTabMode(item.getItemId()));
-            return true;
-        });
-        navigationView.setSelectedItemId(R.id.action_home); //default
+        initBottomNavigationView();
+        initBottomSheetView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_filter:
+                BottomSheetBehavior behavior = mBottomSheetBehavior;
+                if (behavior != null) {
+                    behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -47,11 +65,11 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     public void render(MainUiModel uiModel) {
         Timber.i("render: %s", uiModel);
         if (uiModel instanceof MainUiModel.BoxOffice) {
-            commit(R.id.container, BoxOfficeFragment.newInstance());
+            commit(R.id.tab_container, BoxOfficeFragment.newInstance());
         } else if (uiModel instanceof MainUiModel.Home) {
-            commit(R.id.container, HomeFragment.newInstance());
+            commit(R.id.tab_container, HomeFragment.newInstance());
         } else if (uiModel instanceof MainUiModel.Archive) {
-            commit(R.id.container, ArchiveFragment.newInstance());
+            commit(R.id.tab_container, ArchiveFragment.newInstance());
         } else {
             throw new IllegalStateException("Unknown UI Model");
         }
@@ -61,6 +79,42 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         getSupportFragmentManager().beginTransaction()
                 .replace(containerId, fragment, fragment.getClass().getSimpleName())
                 .commit();
+    }
+
+    private void initBottomNavigationView() {
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(item -> {
+            Timber.d("Select %s", item.getTitle());
+            setTitle(item.getTitle());
+            mPresenter.setTabMode(parseToTabMode(item.getItemId()));
+            return true;
+        });
+        navigationView.setSelectedItemId(R.id.action_home); //default
+    }
+
+    private void initBottomSheetView() {
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet));
+        behavior.setHideable(true);
+        behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        behavior.setPeekHeight(getResources().getDimensionPixelSize(R.dimen.bottom_sheet_peek_height));
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                String stateString;
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                //TODO: apply blur
+            }
+        });
+        mBottomSheetBehavior = behavior;
     }
 
     private static @MainContract.TabMode int parseToTabMode(@IdRes int itemId) {
