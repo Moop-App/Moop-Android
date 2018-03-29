945db3f7eae96eb3e7a9d0af93eaebd59cf338d5
Sungyong An <sungyong.an@naverlabs.com>
2018-03-30 01:10:04 +0900

    Common: add static method to get Application object

--- app/src/main/java/soup/movie/MovieApplication.java
+++ app/src/main/java/soup/movie/MovieApplication.java
@@ -4,9 +4,20 @@ import android.app.Application;
 
 public class MovieApplication extends Application {
 
+    private static Application sInstance;
+
+    public MovieApplication() {
+        super();
+        sInstance = this;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         BuildType.init(this);
+    }
+
+    public static Application getInstance() {
+        return sInstance;
     }
 }
