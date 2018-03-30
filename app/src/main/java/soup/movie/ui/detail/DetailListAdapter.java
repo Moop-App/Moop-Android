c9bcc83c0d1c32271e93e443271e2c1a7847e2fa
Sungyong An <sungyong.an@naverlabs.com>
2018-03-30 12:12:52 +0900

    UI: show timetable for only one theater on detail scene(2)

--- app/src/main/java/soup/movie/ui/detail/DetailListAdapter.java
+++ app/src/main/java/soup/movie/ui/detail/DetailListAdapter.java
@@ -2,6 +2,7 @@ package soup.movie.ui.detail;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,60 +11,127 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import soup.movie.R;
+import soup.movie.data.soup.model.Day;
+import soup.movie.data.soup.model.TheaterCode;
+import soup.movie.data.soup.model.TimeTable;
 import soup.movie.data.soup.model.Trailer;
 import soup.movie.data.utils.YouTubeUtil;
+import soup.movie.ui.util.DialogUtil;
 import soup.movie.ui.util.ImageUtil;
 import soup.movie.util.ListUtil;
+import soup.movie.util.function.Consumer;
 
 class DetailListAdapter extends RecyclerView.Adapter<DetailListAdapter.ViewHolder> {
 
-    private final Activity mHost;
+    private static final int TYPE_HEADER = 3;
+    private static final int TYPE_TIMETABLE_NONE = 0;
+    private static final int TYPE_TIMETABLE = 1;
+    private static final int TYPE_TRAILER = 2;
 
+    private final Activity mHost;
+    private final Consumer<List<TheaterCode>> mConsumer;
+
+    private TimeTable mTimeTable;
     private List<Trailer> mItems = new ArrayList<>();
 
-    DetailListAdapter(Activity host) {
+    DetailListAdapter(Activity host, Consumer<List<TheaterCode>> theaterCodeConsumer) {
         mHost = host;
+        mConsumer = theaterCodeConsumer;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_trailer, parent, false);
-        ViewHolder holder = new ViewHolder(view);
-        holder.itemView.setOnClickListener(v -> YouTubeUtil.executeYoutubeApp(
-                mHost, mItems.get(holder.getAdapterPosition()).getId()));
+        View view;
+        ViewHolder holder;
+        switch (viewType) {
+            case TYPE_TIMETABLE_NONE:
+                view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_timetable_none, parent, false);
+                holder = new NoneTimeTableViewHolder(view);
+                View.OnClickListener listener = v ->
+                        DialogUtil.startDialogToSelectTheaters(mHost, mConsumer::accept);
+                holder.itemView.setOnClickListener(listener);
+                ((NoneTimeTableViewHolder)holder).mSelect.setOnClickListener(listener);
+                break;
+            case TYPE_TIMETABLE:
+                view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_timetable, parent, false);
+                holder = new TimeTableViewHolder(view);
+                holder.itemView.setOnClickListener(v -> {});
+//TODO                        DialogUtil.startDialogToSelectTheaters(mHost, mConsumer::accept));
+                break;
+            default:
+                view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_trailer, parent, false);
+                holder = new TrailerViewHolder(view);
+                holder.itemView.setOnClickListener(v -> YouTubeUtil.executeYoutubeApp(
+                        mHost, mItems.get(holder.getAdapterPosition()).getId()));
+        }
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        Trailer item = mItems.get(position);
-        ImageUtil.loadAsync(mHost, holder.mThumbnailView, YouTubeUtil.getThumbnailUrl(item.getId()));
-        holder.mTitleView.setText(item.getTitle());
-        holder.mAuthorView.setText(item.getAuthor());
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        if (position == 0) {
+            if (holder instanceof TimeTableViewHolder) {
+                TimeTableViewHolder holder1 = (TimeTableViewHolder) holder;
+                TimeTable item = mTimeTable;
+                List<Day> days = item.getDayList();
+                if (days != null) {
+                    holder1.empty.setVisibility(View.GONE);
+                    int size = Math.min(3, days.size());
+                    for (int i = 0; i < size; i++) {
+                        Day day = days.get(i);
+                        holder1.dates[i].setText(day.getDate());
+                        holder1.dates[i].setVisibility(View.VISIBLE);
+                        holder1.times[i].setText(StringUtils.join(day.getTimeList(), ", "));
+                        holder1.times[i].setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        } else {
+            TrailerViewHolder trailerViewHolder = (TrailerViewHolder)holder;
+            Trailer item = mItems.get(position - 1);
+            ImageUtil.loadAsync(mHost, trailerViewHolder.mThumbnailView,
+                    YouTubeUtil.getThumbnailUrl(item.getId()));
+            trailerViewHolder.mTitleView.setText(item.getTitle());
+            trailerViewHolder.mAuthorView.setText(item.getAuthor());
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position != 0 ? TYPE_TRAILER : mTimeTable.getDayList() == null ? TYPE_TIMETABLE_NONE : TYPE_TIMETABLE;
     }
 
     @Override
     public int getItemCount() {
-        return ListUtil.size(mItems);
+        return ListUtil.size(mItems) + calcMoreCount(mTimeTable);
     }
 
-    void updateList(@NonNull List<Trailer> newItems) {
+    private int calcMoreCount(TimeTable timeTable) {
+        return timeTable != null ? 1 : 0;
+    }
+
+    void updateList(@Nullable TimeTable timeTable, @Nullable List<Trailer> newItems) {
         DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
             @Override
             public int getOldListSize() {
-                return ListUtil.size(mItems);
+                return ListUtil.size(mItems) + calcMoreCount(mTimeTable);
             }
 
             @Override
             public int getNewListSize() {
-                return ListUtil.size(newItems);
+                return ListUtil.size(newItems) + calcMoreCount(timeTable);
             }
 
             @Override
@@ -76,11 +144,18 @@ class DetailListAdapter extends RecyclerView.Adapter<DetailListAdapter.ViewHolde
                 return false;
             }
         }, false);
+        mTimeTable = timeTable;
         mItems = newItems;
         result.dispatchUpdatesTo(this);
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    abstract class ViewHolder extends RecyclerView.ViewHolder {
+
+        ViewHolder(View view) {
+            super(view);
+        }
+    }
+    class TrailerViewHolder extends ViewHolder {
 
         @BindView(R.id.trailer_thumbnail)
         ImageView mThumbnailView;
@@ -89,7 +164,33 @@ class DetailListAdapter extends RecyclerView.Adapter<DetailListAdapter.ViewHolde
         @BindView(R.id.sub_text)
         TextView mAuthorView;
 
-        ViewHolder(View view) {
+        TrailerViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+    class TimeTableViewHolder extends ViewHolder {
+
+        @BindViews({
+                R.id.time1, R.id.time2, R.id.time3})
+        TextView[] times;
+        @BindViews({
+                R.id.date1, R.id.date2, R.id.date3})
+        TextView[] dates;
+        @BindView(R.id.empty)
+        TextView empty;
+
+        TimeTableViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+    class NoneTimeTableViewHolder extends ViewHolder {
+
+        @BindView(R.id.select)
+        View mSelect;
+
+        NoneTimeTableViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
         }
