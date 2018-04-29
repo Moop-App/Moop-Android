67f70e211016b4c45d8e7009c9f040d3fa1b2615
SOUP <soupyong@gmail.com>
2018-04-29 11:40:57 +0900

    Data: remove service codes for kobis

--- app/src/main/java/soup/movie/data/MovieRepository.java
+++ app/src/main/java/soup/movie/data/MovieRepository.java
@@ -1,15 +1,6 @@
 package soup.movie.data;
 
-import java.util.List;
-
 import io.reactivex.Single;
-import soup.movie.data.kobis.IKobisDataSource;
-import soup.movie.data.kobis.model.DailyBoxOfficeRequest;
-import soup.movie.data.kobis.model.DailyBoxOfficeResult;
-import soup.movie.data.kobis.model.Movie;
-import soup.movie.data.kobis.model.MovieListRequest;
-import soup.movie.data.kobis.model.WeeklyBoxOfficeRequest;
-import soup.movie.data.kobis.model.WeeklyBoxOfficeResult;
 import soup.movie.data.soup.ISoupDataSource;
 import soup.movie.data.soup.model.ArtMovieRequest;
 import soup.movie.data.soup.model.ArtMovieResponse;
@@ -22,29 +13,12 @@ import soup.movie.data.soup.model.PlanMovieResponse;
 import soup.movie.data.soup.model.TimeTableRequest;
 import soup.movie.data.soup.model.TimeTableResponse;
 
-public class MovieRepository implements IKobisDataSource, ISoupDataSource {
+public class MovieRepository implements ISoupDataSource {
 
-    private final IKobisDataSource kobis;
     private final ISoupDataSource soup;
 
-    public MovieRepository(IKobisDataSource kobisDataSource, ISoupDataSource soupDataSource) {
-        kobis = kobisDataSource;
+    public MovieRepository(ISoupDataSource soupDataSource) {
         soup = soupDataSource;
-    }
-
-    @Override
-    public Single<List<Movie>> getMovieList(MovieListRequest movieListRequest) {
-        return kobis.getMovieList(movieListRequest);
-    }
-
-    @Override
-    public Single<DailyBoxOfficeResult> getDailyBoxOfficeList(DailyBoxOfficeRequest dailyBoxOfficeRequest) {
-        return kobis.getDailyBoxOfficeList(dailyBoxOfficeRequest);
-    }
-
-    @Override
-    public Single<WeeklyBoxOfficeResult> getWeeklyBoxOfficeList(WeeklyBoxOfficeRequest weeklyBoxOfficeRequest) {
-        return kobis.getWeeklyBoxOfficeList(weeklyBoxOfficeRequest);
     }
 
     public Single<NowMovieResponse> getNowList(NowMovieRequest request) {
