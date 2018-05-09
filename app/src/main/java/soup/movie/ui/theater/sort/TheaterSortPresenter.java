fb2181eef5fa285b473f1186f5f51ee2dbc8e5f3
SOUP <soupyong@gmail.com>
2018-05-10 03:22:18 +0900

    UI: clean up codes for theater sorting

--- app/src/main/java/soup/movie/ui/theater/sort/TheaterSortPresenter.java
+++ app/src/main/java/soup/movie/ui/theater/sort/TheaterSortPresenter.java
@@ -1,16 +1,9 @@
 package soup.movie.ui.theater.sort;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
-import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import soup.movie.data.MovieRepository;
-import soup.movie.data.model.Area;
-import soup.movie.data.model.CodeRequest;
 import soup.movie.data.model.TheaterCode;
 import soup.movie.di.ActivityScoped;
 import soup.movie.settings.TheaterSetting;
@@ -20,45 +13,17 @@ import soup.movie.ui.BasePresenter;
 public class TheaterSortPresenter extends BasePresenter<TheaterSortContract.View>
         implements TheaterSortContract.Presenter {
 
-    private final MovieRepository movieRepository;
     private final TheaterSetting theaterSetting;
 
     @Inject
-    TheaterSortPresenter(MovieRepository movieRepository, TheaterSetting theaterSetting) {
-        this.movieRepository = movieRepository;
+    TheaterSortPresenter(TheaterSetting theaterSetting) {
         this.theaterSetting = theaterSetting;
     }
 
     @Override
-    protected void initObservable(CompositeDisposable subscriptions) {
-        subscriptions.add(getViewStateObservable()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(view::render));
-    }
-
-    private Single<TheaterSortViewState> getViewStateObservable() {
-        return Single.zip(
-                getAllTheatersObservable(),
-                getSelectedTheatersObservable(),
-                TheaterSortViewState::new);
-    }
-
-    private Single<List<TheaterCode>> getAllTheatersObservable() {
-        return movieRepository.getCodeList(new CodeRequest())
-                .toObservable()
-                .flatMapIterable(response -> {
-                    ArrayList<Area> areas = new ArrayList<>();
-                    areas.addAll(response.getCgvGroup().getList());
-                    areas.addAll(response.getLotteGroup().getList());
-                    areas.addAll(response.getMegaboxGroup().getList());
-                    return areas;
-                })
-                .flatMapIterable(Area::getTheaterList)
-                .toList();
-    }
-
-    private Single<List<TheaterCode>> getSelectedTheatersObservable() {
-        return Single.just(theaterSetting.getFavoriteTheaters());
+    public void attach(TheaterSortContract.View view) {
+        super.attach(view);
+        view.render(new TheaterSortViewState(theaterSetting.getFavoriteTheaters()));
     }
 
     @Override
--- app/src/main/java/soup/movie/ui/theater/sort/TheaterSortViewState.kt
+++ app/src/main/java/soup/movie/ui/theater/sort/TheaterSortViewState.kt
@@ -2,6 +2,4 @@ package soup.movie.ui.theater.sort
 
 import soup.movie.data.model.TheaterCode
 
-data class TheaterSortViewState(
-        val allTheaters: List<TheaterCode>,
-        val selectedTheaters: List<TheaterCode>)
+data class TheaterSortViewState(val selectedTheaters: List<TheaterCode>)
