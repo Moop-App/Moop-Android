47bb3ee6e31f872b7feb2a8679e7d33afe5c5f41
SOUP <soupyong@gmail.com>
2018-08-25 19:36:42 +0900

    Common: Remove apache commons

---
+++ app/src/main/java/soup/movie/settings/TheaterSetting.kt
@@ -0,0 +1,30 @@
+package soup.movie.settings
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import soup.movie.data.model.Theater
+import java.util.*
+
+class TheaterSetting(private val preferences: SharedPreferences) {
+
+    var favoriteTheaters: List<Theater>
+        get() = fromJson(preferences.getString(KEY, DEFAULT_VALUE)).orEmpty()
+        set(theaters) = preferences.edit().putString(KEY, toJson(theaters)).apply()
+
+    companion object {
+
+        private const val KEY = "favorite_theaters"
+        private const val DEFAULT_VALUE = ""
+
+        private val type = object : TypeToken<ArrayList<Theater>>() {}.type
+
+        private fun toJson(theaterCodeList: List<Theater>): String {
+            return Gson().toJson(theaterCodeList)
+        }
+
+        private fun fromJson(jsonStr: String): List<Theater>? {
+            return Gson().fromJson<List<Theater>>(jsonStr, type)
+        }
+    }
+}
