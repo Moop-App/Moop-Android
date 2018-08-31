caa4d5fb901440ec50595d7aed896214935d7d3f
SOUP <soupyong@gmail.com>
2018-08-31 13:42:14 +0900

    UI: Change MovieListAdapter to DataBnidingAdapter

--- app/src/main/java/soup/movie/ui/main/MovieListAdapter.kt
+++ app/src/main/java/soup/movie/ui/main/MovieListAdapter.kt
@@ -5,21 +5,23 @@ import android.content.Intent
 import android.util.Pair
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_movie_vertical.view.*
 import soup.movie.R
 import soup.movie.data.getColorAsAge
 import soup.movie.data.model.Movie
+import soup.movie.ui.databinding.DataBindingAdapter
+import soup.movie.ui.databinding.DataBindingViewHolder
 import soup.movie.ui.detail.DetailActivity
-import soup.movie.util.*
+import soup.movie.util.AlwaysDiffCallback
+import soup.movie.util.getColorStateListCompat
+import soup.movie.util.loadAsync
+import soup.movie.util.saveTo
 
-class MovieListAdapter(
-        private val host: FragmentActivity)
-    : ListAdapter<Movie, MovieListAdapter.MovieViewHolder>(AlwaysDiffCallback()) {
+class MovieListAdapter(private val host: FragmentActivity) :
+        DataBindingAdapter<Movie>(AlwaysDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder =
-            MovieViewHolder(parent).apply {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<Movie> =
+            super.onCreateViewHolder(parent, viewType).apply {
                 itemView.setOnClickListener { _ ->
                     val intent = Intent(host, DetailActivity::class.java).also {
                         getItem(adapterPosition).saveTo(it)
@@ -33,17 +35,14 @@ class MovieListAdapter(
                 }
             }
 
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        holder.bindItem(getItem(position))
-    }
-
-    class MovieViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
-            parent.inflate(R.layout.item_movie_vertical)) {
-
-        fun bindItem(movie: Movie) {
-            val ctx = itemView.context
-            itemView.posterView.loadAsync(movie.poster)
-            itemView.ageBgView.backgroundTintList = ctx.getColorStateListCompat(movie.getColorAsAge())
+    override fun onBindViewHolder(holder: DataBindingViewHolder<Movie>, position: Int) {
+        super.onBindViewHolder(holder, position)
+        holder.itemView.run {
+            val item = getItem(position)
+            posterView.loadAsync(item.poster)
+            ageBgView.backgroundTintList = context.getColorStateListCompat(item.getColorAsAge())
         }
     }
+
+    override fun getItemViewType(position: Int) = R.layout.item_movie_vertical
 }
