c2c79e18c93325649f5c5e542a5c33f20145a23e
SOUP <soupyong@gmail.com>
2018-09-05 02:54:16 +0900

    App: Remove CrashlyticsTree on debug mode

--- app/src/debug/java/soup/movie/BuildType.kt
+++ app/src/debug/java/soup/movie/BuildType.kt
@@ -5,14 +5,12 @@ import com.facebook.stetho.Stetho
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import okhttp3.OkHttpClient.Builder
 import okhttp3.logging.HttpLoggingInterceptor
-import soup.movie.util.log.CrashlyticsTree
 import timber.log.Timber
 
 object BuildType {
 
     fun init(context: Context) {
         Timber.plant(Timber.DebugTree())
-        Timber.plant(CrashlyticsTree())
         Stetho.initializeWithDefaults(context)
     }
 
