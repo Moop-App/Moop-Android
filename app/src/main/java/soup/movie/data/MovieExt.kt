364d8c5a4820cb831ff212fbbbd51e0253dc0147
Sungyong An <sungyong.an@naverlabs.com>
2018-09-05 11:43:39 +0900

    "TODO" UI: Implements D-Day function simply

--- app/src/main/java/soup/movie/data/MovieExt.kt
+++ app/src/main/java/soup/movie/data/MovieExt.kt
@@ -3,6 +3,7 @@ package soup.movie.data
 import androidx.annotation.ColorRes
 import soup.movie.R
 import soup.movie.data.model.Movie
+import soup.movie.util.TimeUtil
 
 @ColorRes
 fun Movie.getColorAsAge(): Int = when (age) {
@@ -20,3 +21,20 @@ fun Movie.getSimpleAgeLabel(): String = when (age) {
     "청소년관람불가" -> "청불"
     else -> "미정"
 }
+
+fun Movie.getRemainDayLabel(): String = when (getRemainDay()) {
+    UNKNOWN_REMAIN_DAY -> ""
+    else -> "D-$this"
+}
+
+fun Movie.isInOneWeek(): Boolean = getRemainDay() <= 7
+
+private fun Movie.getRemainDay(): Int {
+    val date = openDate.split(".")
+    return when {
+        date.size != 3 -> UNKNOWN_REMAIN_DAY
+        else -> TimeUtil.remainDayTo(date[0].toInt(), date[1].toInt(), date[2].toInt())
+    }
+}
+
+private const val UNKNOWN_REMAIN_DAY = 9999
