6d3cc809bd2eae87a904218e130589eb03d4f79d
SOUP <soupyong@gmail.com>
2018-09-16 13:25:10 +0900

    UI: Prepare to handle menu in tab fragment

--- app/src/main/java/soup/movie/ui/main/BaseTabFragment.kt
+++ app/src/main/java/soup/movie/ui/main/BaseTabFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.annotation.MenuRes
-
 import soup.movie.ui.BaseContract.Presenter
 import soup.movie.ui.BaseContract.View
 import soup.movie.ui.BaseFragment
@@ -12,26 +11,17 @@ import soup.movie.ui.BaseFragment
 abstract class BaseTabFragment<V: View, P: Presenter<V>> :
         BaseFragment<V, P>() {
 
-    protected val menuResource: Int
-        @MenuRes
-        get() = INVALID
+    @MenuRes
+    protected open val menuResource: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setHasOptionsMenu(menuResource != INVALID)
+        setHasOptionsMenu(menuResource != null)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        val menuRes = menuResource
-        if (menuRes != INVALID) {
-            inflater!!.inflate(menuRes, menu)
-        }
+        menuResource?.let { inflater?.inflate(it, menu) }
         super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    companion object {
-
-        private const val INVALID = 0
     }
 
     interface OnReselectListener {
