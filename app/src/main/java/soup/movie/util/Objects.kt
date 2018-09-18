c1fdaa680ad240b35d05f1bcb0336bbb870180df
SOUP <soupyong@gmail.com>
2018-09-19 00:45:04 +0900

    Common: Fix crash by fromJson() in Objects

--- app/src/main/java/soup/movie/util/Objects.kt
+++ app/src/main/java/soup/movie/util/Objects.kt
@@ -4,4 +4,8 @@ import com.google.gson.Gson
 
 inline fun <reified T : Any> T.toJson(): String = Gson().toJson(this, T::class.java)
 
-inline fun <reified T : Any> String.fromJson(): T? = Gson().fromJson(this, T::class.java)
+inline fun <reified T : Any> String.fromJson(): T? = try {
+    Gson().fromJson(this, T::class.java)
+} catch (t: Throwable) {
+    null
+}
