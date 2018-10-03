cb2c1f9760ab2091243e265d5eec9a2b47e31d7b
SOUP <soupyong@gmail.com>
2018-10-04 02:42:08 +0900

    Data: Clean up codes to get app version

--- data/src/main/java/soup/movie/data/source/remote/RemoteMoobDataSource.kt
+++ data/src/main/java/soup/movie/data/source/remote/RemoteMoobDataSource.kt
@@ -2,7 +2,6 @@ package soup.movie.data.source.remote
 
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
-import org.jsoup.Jsoup
 import soup.movie.data.model.Movie
 import soup.movie.data.model.Theater
 import soup.movie.data.model.Timetable
@@ -27,23 +26,6 @@ class RemoteMoobDataSource(private val moobApiService: MoobApiService) : MoobDat
                     .map { it.timetable }
 
     override fun getVersion(pkgName: String, defaultVersion: String): Observable<Version> =
-            Observable.fromCallable {
-                Version(getMarketVersionName(pkgName) ?: defaultVersion)
-            }.subscribeOn(Schedulers.io())
-
-    private fun getMarketVersionName(pkgName: String): String? {
-        return try {
-            Jsoup.connect("https://play.google.com/store/apps/details?id=$pkgName&hl=it")
-                    .timeout(10000)
-                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
-                    .referrer("http://www.google.com")
-                    .get()
-                    .select("div[itemprop=softwareVersion]")
-                    .first()
-                    .ownText()
-        } catch (e: Exception) {
-            null
-        }
-
-    }
+            Observable.just(Version(defaultVersion))
+                    .subscribeOn(Schedulers.io())
 }
