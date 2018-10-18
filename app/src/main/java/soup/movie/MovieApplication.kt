8051393419c8391623056b3cee8f3a17eb540ba8
SOUP <soupyong@gmail.com>
2018-10-19 02:53:23 +0900

    App: Initialize Crashlytics in release build only

--- app/src/main/java/soup/movie/MovieApplication.kt
+++ app/src/main/java/soup/movie/MovieApplication.kt
@@ -1,12 +1,9 @@
 package soup.movie
 
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.mapbox.mapboxsdk.Mapbox
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
-import io.fabric.sdk.android.Fabric
 import soup.movie.di.DaggerApplicationComponent
 import soup.movie.spec.ThemeSpecs
 import soup.movie.theme.ThemeBook
@@ -16,20 +13,12 @@ class MovieApplication : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         BuildType.init(this)
-        initCrashlytics()
         AndroidThreeTen.init(this)
         ThemeBook.initialize(this,
                 ThemeSpecs.DEFAULT,
                 ThemeSpecs.BLACK,
                 ThemeSpecs.WA_TGBH)
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
-    }
-
-    private fun initCrashlytics() {
-        val core = CrashlyticsCore.Builder()
-                .disabled(BuildConfig.DEBUG)
-                .build()
-        Fabric.with(this, Crashlytics.Builder().core(core).build())
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
--- app/src/release/java/soup/movie/BuildType.kt
+++ app/src/release/java/soup/movie/BuildType.kt
@@ -1,6 +1,9 @@
 package soup.movie
 
 import android.content.Context
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
 import okhttp3.OkHttpClient.Builder
 import soup.movie.util.log.CrashlyticsTree
 import timber.log.Timber
@@ -9,6 +12,14 @@ object BuildType {
 
     fun init(context: Context) {
         Timber.plant(CrashlyticsTree())
+        initCrashlytics(context)
+    }
+
+    private fun initCrashlytics(context: Context) {
+        val core = CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG)
+                .build()
+        Fabric.with(context, Crashlytics.Builder().core(core).build())
     }
 
     fun addNetworkInterceptor(builder: Builder): Builder = builder
