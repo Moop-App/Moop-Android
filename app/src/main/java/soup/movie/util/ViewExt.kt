28e0da7ed78459753ae4809629379bb40523be89
SOUP <soupyong@gmail.com>
2019-05-02 22:15:21 +0900

    UI: Clean up View Extensions codes

--- app/src/main/java/soup/movie/util/ViewExt.kt
+++ app/src/main/java/soup/movie/util/ViewExt.kt
@@ -17,23 +17,19 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
-import soup.movie.data.helper.today
-import soup.movie.util.Interpolators.ALPHA_IN
-import soup.movie.util.Interpolators.ALPHA_OUT
 import soup.movie.util.glide.GlideApp
-import soup.movie.util.glide.IntegerVersionSignature
 
 /** View */
 
-fun View.blockExtraTouchEvents() = setOnTouchListener { _, _ ->  true }
+fun View.blockExtraTouchEvents() = setOnTouchListener { _, _ -> true }
 
 @Suppress("UNCHECKED_CAST")
 fun <T : View> inflate(context: Context, @LayoutRes resource: Int): T =
-        View.inflate(context, resource, null) as T
+    View.inflate(context, resource, null) as T
 
 @Suppress("UNCHECKED_CAST")
 fun <T : View> inflate(context: Context, @LayoutRes resource: Int, root: ViewGroup): T =
-        View.inflate(context, resource, root) as T
+    View.inflate(context, resource, root) as T
 
 inline fun View.setVisibleIf(predicate: () -> Boolean) {
     visibility = if (predicate()) View.VISIBLE else View.GONE
@@ -53,40 +49,6 @@ fun View.setGoneIfInXml(predicate: Boolean) {
     visibility = if (predicate) View.VISIBLE else View.GONE
 }
 
-fun View.animateHide(animate: Boolean) {
-    animate().cancel()
-    if (!animate) {
-        alpha = 0f
-        visibility = View.INVISIBLE
-        return
-    }
-    animate()
-            .alpha(0f)
-            .setDuration(160)
-            .setStartDelay(0)
-            .setInterpolator(ALPHA_OUT)
-            .withEndAction { visibility = View.INVISIBLE }
-}
-
-fun View.animateShow(animate: Boolean) {
-    animate().cancel()
-    visibility = View.VISIBLE
-    if (!animate) {
-        alpha = 1f
-        return
-    }
-    alpha = 0f
-    animate()
-            .alpha(1f)
-            .setDuration(320)
-            .setInterpolator(ALPHA_IN)
-            .setStartDelay(50)
-            // We need to clean up any pending end action from animateHide if we call
-            // both hide and show in the same frame before the animation actually gets started.
-            // cancel() doesn't really remove the end action.
-            .withEndAction(null)
-}
-
 fun View.setBackgroundColorResource(@ColorRes resId: Int) {
     setBackgroundColor(context.getColorCompat(resId))
 }
@@ -94,11 +56,6 @@ fun View.setBackgroundColorResource(@ColorRes resId: Int) {
 @BindingAdapter("android:backgroundResource")
 fun View.setBackgroundResourceInXml(resId: Int) {
     setBackgroundResource(resId)
-}
-
-@BindingAdapter("android:selected")
-fun View.setSelectedInXml(selected: Boolean) {
-    isSelected = selected
 }
 
 infix fun View.with(tag: String): Pair<View, String> {
@@ -117,7 +74,7 @@ fun View.setBackgroundTint(@ColorInt color: Int) {
 /** ViewGroup */
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View =
-        LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+    LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
 
 /** ImageView */
 
@@ -129,20 +86,18 @@ fun ImageView.setImageTint(@ColorInt color: Int) {
 @BindingAdapter("android:srcUrl")
 fun ImageView.loadAsync(url: String) {
     GlideApp.with(context)
-            .load(url)
-            .signature(IntegerVersionSignature(today().run { year * 100 + monthValue }))
-            .priority(Priority.IMMEDIATE)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(this)
+        .load(url)
+        .priority(Priority.IMMEDIATE)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(this)
 }
 
 fun ImageView.loadAsync(url: String, requestListener: RequestListener<Drawable>) {
     GlideApp.with(context)
-            .load(url)
-            .signature(IntegerVersionSignature(today().run { year * 100 + monthValue }))
-            .listener(requestListener)
-            .priority(Priority.IMMEDIATE)
-            .into(this)
+        .load(url)
+        .listener(requestListener)
+        .priority(Priority.IMMEDIATE)
+        .into(this)
 }
 
 /* ContentLoadingProgressBar */
