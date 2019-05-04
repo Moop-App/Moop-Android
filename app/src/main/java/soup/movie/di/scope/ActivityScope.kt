fa4c22c80bcd7b41dcdad2ea6483315df36c15b5
SOUP <soupyong@gmail.com>
2019-05-05 00:04:15 +0900

    App: Convert scope classes to kotlin

---
+++ app/src/main/java/soup/movie/di/scope/ActivityScope.kt
@@ -0,0 +1,8 @@
+package soup.movie.di.scope
+
+import javax.inject.Scope
+
+@Scope
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
---
+++ app/src/main/java/soup/movie/di/scope/ChildFragmentScope.kt
@@ -0,0 +1,14 @@
+package soup.movie.di.scope
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FILE,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+annotation class ChildFragmentScope
