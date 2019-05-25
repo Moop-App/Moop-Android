69bf559ff05278cacbe869dce5357df6ee91585f
SOUP <soupyong@gmail.com>
2019-05-25 19:24:29 +0900

    UI: Clean up DetailViewBindingAdapter codes

--- app/src/main/java/soup/movie/ui/detail/DetailViewBindingAdapter.kt
+++ app/src/main/java/soup/movie/ui/detail/DetailViewBindingAdapter.kt
@@ -1,8 +1,8 @@
 package soup.movie.ui.detail
 
-import android.widget.TextView
 import androidx.core.view.isEmpty
 import androidx.databinding.BindingAdapter
+import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import soup.movie.R
 import soup.movie.util.inflate
@@ -11,9 +11,9 @@ import soup.movie.util.inflate
 fun ChipGroup.setGenreList(genreList: List<String>) {
     if (isEmpty()) {
         genreList.forEach {
-            val textView: TextView = inflate(context, R.layout.chip_genre)
-            textView.text = it
-            addView(textView)
+            val chip: Chip = inflate(context, R.layout.chip_action_genre)
+            chip.text = it
+            addView(chip)
         }
     }
 }
