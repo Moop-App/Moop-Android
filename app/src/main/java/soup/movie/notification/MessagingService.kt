02d72eada1484fe6ec74cb9dc2d54c837e0419aa
SOUP <soupyong@gmail.com>
2019-05-26 19:23:50 +0900

    App: Revert hard-coded description for culture day

--- app/src/main/java/soup/movie/notification/MessagingService.kt
+++ app/src/main/java/soup/movie/notification/MessagingService.kt
@@ -6,8 +6,6 @@ import android.content.Intent
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import soup.movie.R
-import soup.movie.data.helper.isInWeekOfCultureDay
-import soup.movie.data.helper.today
 import soup.movie.notification.NotificationSpecs.TYPE_EVENT
 import soup.movie.notification.NotificationSpecs.TYPE_NOTICE
 import soup.movie.ui.main.MainActivity
@@ -31,7 +29,7 @@ class MessagingService : FirebaseMessagingService() {
             if (type != null && title != null && text != null) {
                 when (type) {
                     TYPE_NOTICE -> notifyNotice(title = title, text = text)
-                    TYPE_EVENT -> notifyEvent(originTitle = title, originText = text)
+                    TYPE_EVENT -> notifyEvent(title = title, text = text)
                 }
             }
         }
@@ -48,17 +46,7 @@ class MessagingService : FirebaseMessagingService() {
         }
     }
 
-    private fun Context.notifyEvent(originTitle: String, originText: String) {
-        val title: String
-        val text: String
-        if (today().isInWeekOfCultureDay()) {
-            //TODO: hard-coded description
-            title = "문화의 날"
-            text = "매달 마지막 수요일은 문화의 날입니다.\n이번주 상영하는 영화를 확인해보세요!"
-        } else {
-            title = originTitle
-            text = originText
-        }
+    private fun Context.notifyEvent(title: String, text: String) {
         NotificationSpecs.notifyAsEvent(this) {
             setSmallIcon(R.drawable.ic_notify_default)
             setContentTitle(title)
