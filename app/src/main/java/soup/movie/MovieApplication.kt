8faed6f32641794d94100bf96a58c96b93463a48
SOUP <soupyong@gmail.com>
2019-05-28 00:27:41 +0900

    UI: Initialize night mode if starts application

--- app/src/main/java/soup/movie/MovieApplication.kt
+++ app/src/main/java/soup/movie/MovieApplication.kt
@@ -3,16 +3,22 @@ package soup.movie
 import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import soup.movie.data.ThemeOptionManager
 import soup.movie.di.DaggerApplicationComponent
 import soup.movie.notification.NotificationSpecs
+import javax.inject.Inject
 
 class MovieApplication : DaggerApplication() {
+
+    @Inject
+    lateinit var themeOptionManager: ThemeOptionManager
 
     override fun onCreate() {
         super.onCreate()
         BuildType.init(this)
         AndroidThreeTen.init(this)
         NotificationSpecs.initialize(this)
+        themeOptionManager.initialize()
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
