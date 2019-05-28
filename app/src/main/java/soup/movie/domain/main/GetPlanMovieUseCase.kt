e20b98034dfd24e79a7e6eadda8f09b8a605e02b
SOUP <soupyong@gmail.com>
2019-05-29 01:15:10 +0900

    #28, Move filter codes into MovieFilter class

--- app/src/main/java/soup/movie/domain/main/GetPlanMovieUseCase.kt
+++ app/src/main/java/soup/movie/domain/main/GetPlanMovieUseCase.kt
@@ -3,10 +3,7 @@ package soup.movie.domain.main
 import io.reactivex.Observable
 import soup.movie.data.MoopRepository
 import soup.movie.data.helper.getDDay
-import soup.movie.data.model.AgeFilter
-import soup.movie.data.model.GenreFilter
 import soup.movie.data.model.Movie
-import soup.movie.data.model.TheaterFilter
 import soup.movie.domain.model.MovieFilter
 import soup.movie.ui.main.movie.MovieListUiModel
 
@@ -22,31 +19,11 @@ class GetPlanMovieUseCase(
             .map { response ->
                 response.list.asSequence()
                     .sortedBy(Movie::getDDay)
-                    .filter { it.isFilterBy(movieFilter.theaterFilter) }
-                    .filter { it.isFilterBy(movieFilter.ageFilter) }
-                    .filter { it.isFilterBy(movieFilter.genreFilter) }
+                    .filter { movieFilter(it) }
                     .toList()
             }
             .map { MovieListUiModel.DoneState(it) as MovieListUiModel }
             .startWith(MovieListUiModel.LoadingState)
             .onErrorReturnItem(MovieListUiModel.ErrorState)
-    }
-
-    private fun Movie.isFilterBy(theaterFilter: TheaterFilter): Boolean {
-        return (theaterFilter.hasCgv() and isScreeningAtCgv()) or
-            (theaterFilter.hasLotteCinema() and isScreeningAtLotteCinema()) or
-            (theaterFilter.hasMegabox() and isScreeningAtMegabox())
-    }
-
-    private fun Movie.isFilterBy(ageFilter: AgeFilter): Boolean {
-        return (ageFilter.hasAll() and isScreeningForAgeAll()) or
-            (ageFilter.has12() and isScreeningOverAge12()) or
-            (ageFilter.has15() and isScreeningOverAge15()) or
-            (ageFilter.has19() and isScreeningOverAge19())
-    }
-
-    private fun Movie.isFilterBy(genreFilter: GenreFilter): Boolean {
-        return genre?.any { it !in genreFilter.blacklist }
-            ?: genre.isNullOrEmpty() and genreFilter.isEtcIncluded()
     }
 }
--- app/src/main/java/soup/movie/domain/model/MovieFilter.kt
+++ app/src/main/java/soup/movie/domain/model/MovieFilter.kt
@@ -2,10 +2,43 @@ package soup.movie.domain.model
 
 import soup.movie.data.model.AgeFilter
 import soup.movie.data.model.GenreFilter
+import soup.movie.data.model.Movie
 import soup.movie.data.model.TheaterFilter
 
 class MovieFilter(
-    val theaterFilter: TheaterFilter,
-    val ageFilter: AgeFilter,
-    val genreFilter: GenreFilter
-)
+    private val theaterFilter: TheaterFilter,
+    private val ageFilter: AgeFilter,
+    private val genreFilter: GenreFilter
+) {
+
+    operator fun invoke(movie: Movie): Boolean {
+        return movie.isFilterBy(theaterFilter)
+            && movie.isFilterBy(ageFilter)
+            && movie.isFilterBy(genreFilter)
+    }
+
+    private fun Movie.isFilterBy(theaterFilter: TheaterFilter): Boolean {
+        fun isScreeningAtCgv(): Boolean = cgv != null
+        fun isScreeningAtLotteCinema(): Boolean = lotte != null
+        fun isScreeningAtMegabox(): Boolean = megabox != null
+        return theaterFilter.hasCgv() and isScreeningAtCgv()
+            || theaterFilter.hasLotteCinema() and isScreeningAtLotteCinema()
+            || theaterFilter.hasMegabox() and isScreeningAtMegabox()
+    }
+
+    private fun Movie.isFilterBy(ageFilter: AgeFilter): Boolean {
+        fun isScreeningForAgeAll(): Boolean = ageValue < 12
+        fun isScreeningOverAge12(): Boolean = (12 <= ageValue) and (ageValue < 15)
+        fun isScreeningOverAge15(): Boolean = (15 <= ageValue) and (ageValue < 19)
+        fun isScreeningOverAge19(): Boolean = 19 <= ageValue
+        return (ageFilter.hasAll() and isScreeningForAgeAll())
+            || (ageFilter.has12() and isScreeningOverAge12())
+            || (ageFilter.has15() and isScreeningOverAge15())
+            || (ageFilter.has19() and isScreeningOverAge19())
+    }
+
+    private fun Movie.isFilterBy(genreFilter: GenreFilter): Boolean {
+        return genre?.any { it !in genreFilter.blacklist }
+            ?: genre.isNullOrEmpty() and genreFilter.isEtcIncluded()
+    }
+}
