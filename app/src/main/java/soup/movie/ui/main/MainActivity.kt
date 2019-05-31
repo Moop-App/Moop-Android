17eb894186b29864c45533d0b6691759ad57db30
SOUP <soupyong@gmail.com>
2019-06-01 00:56:02 +0900

    App: Use navigation for main scene

--- app/src/main/java/soup/movie/ui/main/MainActivity.kt
+++ app/src/main/java/soup/movie/ui/main/MainActivity.kt
@@ -5,13 +5,14 @@ import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.os.Bundle
 import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.android.synthetic.main.activity_main.*
 import soup.movie.R
 import soup.movie.analytics.EventAnalytics
 import soup.movie.data.MovieSelectManager
 import soup.movie.spec.KakaoLink
 import soup.movie.ui.BaseActivity
+import soup.movie.ui.base.OnBackPressedListener
 import soup.movie.ui.detail.DetailActivity
-import soup.movie.ui.home.HomeFragment
 import soup.movie.util.observeEvent
 import javax.inject.Inject
 
@@ -26,11 +27,6 @@ class MainActivity : BaseActivity() {
         setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.container, HomeFragment.newInstance())
-                .commit()
-        }
 
         //TODO: Improve this please
         FirebaseMessaging.getInstance().isAutoInitEnabled = true
@@ -67,5 +63,18 @@ class MainActivity : BaseActivity() {
                     .toBundle())
             }
         }
+    }
+
+    override fun onBackPressed() {
+        if (handleBackEventInChildFragment()) return
+        super.onBackPressed()
+    }
+
+    private fun handleBackEventInChildFragment(): Boolean {
+        val current = navHostFragment.childFragmentManager.fragments.elementAtOrNull(0)
+        if (current is OnBackPressedListener) {
+            return current.onBackPressed()
+        }
+        return false
     }
 }
