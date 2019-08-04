918276081e568a781dd554aadb2a08a3e6769a1e
SOUP <soupyong@gmail.com>
2019-08-04 18:59:21 +0900

    Data: Convert html string properly

--- data/src/main/java/soup/movie/data/model/Trailer.kt
+++ data/src/main/java/soup/movie/data/model/Trailer.kt
@@ -1,11 +1,17 @@
 package soup.movie.data.model
 
 import androidx.annotation.Keep
+import androidx.core.text.HtmlCompat
+import com.google.gson.annotations.SerializedName
 
 @Keep
 data class Trailer(
     val youtubeId: String,
-    val title: String,
+    @SerializedName("title")
+    private val _title: String,
     val author: String,
     val thumbnailUrl: String
-)
+) {
+    val title: String
+        get() = HtmlCompat.fromHtml(_title, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+}
