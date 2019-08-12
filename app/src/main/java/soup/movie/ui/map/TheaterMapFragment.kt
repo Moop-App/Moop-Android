593360e3bffd7fac06a636d1a432ed2efc2635cf
Sungyong An <soupyong@gmail.com>
2019-08-12 20:53:16 +0900

    #67, App: Setup default settings for NaverMap

--- app/src/main/java/soup/movie/ui/map/TheaterMapFragment.kt
+++ app/src/main/java/soup/movie/ui/map/TheaterMapFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.updatePadding
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.util.FusedLocationSource
+import soup.movie.R
 import soup.movie.databinding.TheaterMapFragmentBinding
 import soup.movie.ui.base.BaseFragment
 import soup.movie.ui.main.MainViewModel
@@ -16,6 +20,8 @@ class TheaterMapFragment : BaseFragment() {
 
     private val activityViewModel: MainViewModel by activityViewModels()
     private val viewModel: TheaterMapViewModel by viewModels()
+
+    private lateinit var locationSource: FusedLocationSource
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,13 +42,37 @@ class TheaterMapFragment : BaseFragment() {
                 activityViewModel.openNavigationMenu()
             }
         }
+
+        locationSource = FusedLocationSource(this@TheaterMapFragment, LOCATION_PERMISSION_REQUEST_CODE)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.naverMapFragment) as MapFragment
+        mapFragment.getMapAsync { naverMap ->
+            naverMap.locationSource = locationSource
+            naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        }
     }
 
     private fun TheaterMapFragmentBinding.adaptSystemWindowInset() {
         theaterMapScene.doOnApplyWindowInsets { view, windowInsets, initialPadding ->
             view.updatePadding(
-                top = initialPadding.top + windowInsets.systemWindowInsetTop
+                top = initialPadding.top + windowInsets.systemWindowInsetTop,
+                bottom = initialPadding.bottom + windowInsets.systemWindowInsetBottom
             )
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
