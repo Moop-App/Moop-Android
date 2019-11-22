a4666ba2d2f52cf1e199293ac38ac39900a753de
Sungyong An <soupyong@gmail.com>
2019-11-22 23:40:46 +0900

    UI: Make ViewPager2 less sensitive

---
+++ widget/src/main/java/androidx/viewpager2/NestedScrollableHost.kt
@@ -0,0 +1,59 @@
+package androidx.viewpager2
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import android.widget.FrameLayout
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.ORIENTATION_HORIZONTAL
+import kotlin.math.absoluteValue
+
+/**
+ * @link https://android-review.googlesource.com/c/platform/frameworks/support/+/1131083/
+ */
+class NestedScrollableHost : FrameLayout {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    private var touchSlop = 0
+    private var initialX = 0f
+    private var initialY = 0f
+    private val parentViewPager: ViewPager2?
+        get() {
+            var v: View? = parent as? View
+            while (v != null && v !is ViewPager2) {
+                v = v.parent as? View
+            }
+            return v as? ViewPager2
+        }
+
+    init {
+        touchSlop = ViewConfiguration.get(context).scaledPagingTouchSlop * 2
+    }
+
+    override fun onInterceptTouchEvent(e: MotionEvent): Boolean {
+        handleInterceptTouchEvent(e)
+        return super.onInterceptTouchEvent(e)
+    }
+
+    private fun handleInterceptTouchEvent(e: MotionEvent) {
+        val orientation = parentViewPager?.orientation ?: return
+        if (e.action == MotionEvent.ACTION_DOWN) {
+            initialX = e.x
+            initialY = e.y
+            parent.requestDisallowInterceptTouchEvent(true)
+        } else if (e.action == MotionEvent.ACTION_MOVE) {
+            val dx = e.x - initialX
+            val dy = e.y - initialY
+            val isVpHorizontal = orientation == ORIENTATION_HORIZONTAL
+
+            // assuming ViewPager2 touch-slop is 5x touch-slop of child
+            val scaledDx = dx.absoluteValue * if (isVpHorizontal) 1f else .2f
+            val scaledDy = dy.absoluteValue * if (isVpHorizontal) .2f else 1f
+            val disallowIntercept = scaledDx <= scaledDy || scaledDx <= touchSlop
+            parent.requestDisallowInterceptTouchEvent(disallowIntercept)
+        }
+    }
+}
