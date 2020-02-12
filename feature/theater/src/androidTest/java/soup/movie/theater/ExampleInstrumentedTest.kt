d975ce7d0e241b05b2a337b73a091bd951d83c3a
Sungyong An <soupyong@gmail.com>
2020-02-13 00:53:05 +0900

    App: Clean up codes

--- feature/theater/src/androidTest/java/soup/movie/theater/ExampleInstrumentedTest.kt
+++ feature/theater/src/androidTest/java/soup/movie/theater/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package soup.movie.theater.edit
+package soup.movie.theater
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("soup.movie.theater.edit.test", appContext.packageName)
+        assertEquals("soup.movie.theater.test", appContext.packageName)
     }
 }
--- feature/theater/src/test/java/soup/movie/theater/ExampleUnitTest.kt
+++ feature/theater/src/test/java/soup/movie/theater/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package soup.movie.theater.edit
+package soup.movie.theater
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
