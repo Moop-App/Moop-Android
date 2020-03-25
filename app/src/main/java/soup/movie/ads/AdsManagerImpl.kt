5c32e5f5ad800fbda29266ec46802b57a1fc1122
Sungyong An <soupyong@gmail.com>
2020-03-25 18:22:38 +0900

    App: Try to fix native ad are not shown in release

--- app/src/main/java/soup/movie/ads/AdsManagerImpl.kt
+++ app/src/main/java/soup/movie/ads/AdsManagerImpl.kt
@@ -4,12 +4,18 @@ import android.content.Context
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.formats.UnifiedNativeAd
 import soup.movie.R
+import timber.log.Timber
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class AdsManagerImpl(private val context: Context) : AdsManager {
+
+    init {
+        MobileAds.initialize(context)
+    }
 
     private val adUnitId: String = context.getString(R.string.admob_ad_unit_detail)
 
@@ -19,6 +25,7 @@ class AdsManagerImpl(private val context: Context) : AdsManager {
                 .forUnifiedNativeAd { continuation.resume(it) }
                 .withAdListener(object : AdListener() {
                     override fun onAdFailedToLoad(errorCode: Int) {
+                        Timber.w("onAdFailedToLoad: errorCode=$errorCode")
                         continuation.resume(null)
                     }
                 })
