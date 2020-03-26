f08a22aed896c798aa17b72763a5f11a0a3b5954
Sungyong An <soupyong@gmail.com>
2020-03-26 23:48:01 +0900

    App: Upgrade to the Firebase Crashlytics SDK
    https://firebase.google.com/docs/crashlytics/upgrade-sdk?platform=android

--- app/src/release/java/soup/movie/BuildType.kt
+++ app/src/release/java/soup/movie/BuildType.kt
@@ -1,22 +1,11 @@
 package soup.movie
 
 import android.content.Context
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
-import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 object BuildType {
 
     fun init(context: Context) {
         Timber.plant(CrashlyticsTree())
-        initCrashlytics(context)
-    }
-
-    private fun initCrashlytics(context: Context) {
-        val core = CrashlyticsCore.Builder()
-                .disabled(BuildConfig.DEBUG)
-                .build()
-        Fabric.with(context, Crashlytics.Builder().core(core).build())
     }
 }
--- app/src/release/java/soup/movie/CrashlyticsTree.kt
+++ app/src/release/java/soup/movie/CrashlyticsTree.kt
@@ -1,19 +1,21 @@
 package soup.movie
 
 import android.util.Log
-import com.crashlytics.android.Crashlytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 
 class CrashlyticsTree : Timber.Tree() {
+
+    private val crashlytics = FirebaseCrashlytics.getInstance()
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
             return
         }
         when {
-            t != null -> Crashlytics.logException(t)
-            tag != null -> Crashlytics.log("$tag: $message")
-            else -> Crashlytics.log(message)
+            t != null -> crashlytics.recordException(t)
+            tag != null -> crashlytics.log("$tag: $message")
+            else -> crashlytics.log(message)
         }
     }
 }
