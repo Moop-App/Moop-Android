475129951075211a468f5fd487156e2a7317f648
fornewid <soupyong@gmail.com>
2021-07-20 00:18:42 +0900

    Run spotlessApply

--- app/src/main/java/soup/movie/config/RemoteConfig.kt
+++ app/src/main/java/soup/movie/config/RemoteConfig.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 SOUP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package soup.movie.config
 
 import com.google.firebase.ktx.Firebase
@@ -6,7 +21,7 @@ import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import soup.movie.R
 
-class RemoteConfig: Config {
+class RemoteConfig : Config {
 
     private val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig.apply {
         val configSettings = remoteConfigSettings {
