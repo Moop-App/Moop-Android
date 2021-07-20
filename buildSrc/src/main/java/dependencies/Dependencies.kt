d4054cb1e69d6873992983b6056f0d98e78df5df
fornewid <soupyong@gmail.com>
2021-07-20 22:43:39 +0900

    release 1.0.6

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -6,8 +6,8 @@ object Versions {
     const val targetSdk = 30
     const val compileSdk = 30
     const val buildTools = "30.0.2"
-    const val versionCode = 105
-    const val versionName = "1.0.5"
+    const val versionCode = 106
+    const val versionName = "1.0.6"
 }
 
 object Libs {
