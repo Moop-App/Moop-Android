279f19e7363f71643fae110b9df7af97c313bc0c
fornewid <soupyong@gmail.com>
2021-09-13 00:42:13 +0900

    Run spotlessApply before preBuild

--- feature/theme/src/main/java/soup/movie/theme/internal/ThemeOptionScreen.kt
+++ feature/theme/src/main/java/soup/movie/theme/internal/ThemeOptionScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
