d942d063100bbd7b18b9072a38b0141bbe229491
Sungyong An (SOUP) <soupyong@gmail.com>
2021-11-30 22:07:39 +0900

    Android 12 finally! (#135)
    * Upgrade targetSdkVersion to 31
    
    * Update libraries

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -3,9 +3,8 @@ package dependencies
 object Versions {
     const val appId = "soup.movie"
     const val minSdk = 23
-    const val targetSdk = 30
-    const val compileSdk = 30
-    const val buildTools = "30.0.2"
+    const val targetSdk = 31
+    const val compileSdk = 31
     const val versionCode = 106
     const val versionName = "1.0.6"
 }
@@ -14,11 +13,11 @@ object Libs {
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.3"
 
     // UI
-    const val insetter = "dev.chrisbanes.insetter:insetter:0.6.0"
+    const val insetter = "dev.chrisbanes.insetter:insetter:0.6.1"
     const val photoview = "com.github.chrisbanes:PhotoView:2.3.0"
     const val recyclerViewAnimators = "jp.wasabeef:recyclerview-animators:4.0.2"
-    const val kakaoLink = "com.kakao.sdk:v2-link:2.7.0"
-    const val naverMap = "com.naver.maps:map-sdk:3.12.0"
+    const val kakaoLink = "com.kakao.sdk:v2-link:2.8.3"
+    const val naverMap = "com.naver.maps:map-sdk:3.13.0"
     const val playServicesLocation = "com.google.android.gms:play-services-location:17.1.0"
 
     // Utils
@@ -41,7 +40,7 @@ object Libs {
     }
 
     object Dagger {
-        private const val version = "2.38.1"
+        private const val version = "2.40.2"
         const val hilt = "com.google.dagger:hilt-android:$version"
         const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$version"
         const val hiltTesting = "com.google.dagger:hilt-android-testing:$version"
@@ -49,23 +48,29 @@ object Libs {
     }
 
     object Google {
-        const val gmsPlugin = "com.google.gms:google-services:4.3.8"
-        const val play = "com.google.android.play:core:1.10.0"
+        const val gmsPlugin = "com.google.gms:google-services:4.3.10"
+        const val play = "com.google.android.play:core:1.10.2"
     }
 
     object AndroidX {
-        const val activity = "androidx.activity:activity-ktx:1.3.1"
-        const val annotation = "androidx.annotation:annotation:1.2.0"
-        const val appcompat = "androidx.appcompat:appcompat:1.3.1"
-        const val browser = "androidx.browser:browser:1.3.0"
+        const val activity = "androidx.activity:activity-ktx:1.4.0"
+        const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+        const val annotation = "androidx.annotation:annotation:1.3.0"
+        const val appcompat = "androidx.appcompat:appcompat:1.4.0"
+        const val browser = "androidx.browser:browser:1.4.0"
         const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.1.0"
-        const val core = "androidx.core:core-ktx:1.6.0"
+        const val core = "androidx.core:core-ktx:1.7.0"
         const val drawerlayout = "androidx.drawerlayout:drawerlayout:1.1.1"
-        const val fragment = "androidx.fragment:fragment-ktx:1.3.6"
+        const val fragment = "androidx.fragment:fragment-ktx:1.4.0"
         const val recyclerview = "androidx.recyclerview:recyclerview:1.2.1"
         const val startup = "androidx.startup:startup-runtime:1.1.0"
         const val transition = "androidx.transition:transition:1.4.1"
         const val viewpager2 = "androidx.viewpager2:viewpager2:1.1.0-beta01"
+
+        object DataStore {
+            private const val version = "1.0.0"
+            const val preferences = "androidx.datastore:datastore-preferences:$version"
+        }
 
         object Hilt {
             private const val version = "1.0.0"
@@ -75,7 +80,7 @@ object Libs {
         }
 
         object Lifecycle {
-            private const val version = "2.3.1"
+            private const val version = "2.4.0"
             const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
             const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
             const val process = "androidx.lifecycle:lifecycle-process:$version"
@@ -84,13 +89,14 @@ object Libs {
         }
 
         object Navigation {
-            private const val version = "2.3.5"
+            private const val version = "2.4.0-beta02"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val ui = "androidx.navigation:navigation-ui-ktx:$version"
             const val dynamicFeaturesFragment =
                 "androidx.navigation:navigation-dynamic-features-fragment:$version"
             const val safeArgsPlugin =
                 "androidx.navigation:navigation-safe-args-gradle-plugin:$version"
+            const val compose = "androidx.navigation:navigation-compose:$version"
         }
 
         object Room {
@@ -101,15 +107,9 @@ object Libs {
         }
 
         object WorkManager {
-            private const val version = "2.6.0"
+            private const val version = "2.7.1"
             const val runtime = "androidx.work:work-runtime-ktx:$version"
-            const val gcm = "androidx.work:work-gcm:$version"
             const val testing = "androidx.work:work-testing:$version"
-        }
-
-        object DataStore {
-            private const val version = "1.0.0"
-            const val preferences = "androidx.datastore:datastore-preferences:$version"
         }
 
         object Test {
@@ -123,15 +123,12 @@ object Libs {
 
     object Material {
         const val view = "com.google.android.material:material:1.4.0"
-        const val composeThemeAdapter = "com.google.android.material:compose-theme-adapter:1.0.2"
+        const val composeThemeAdapter = "com.google.android.material:compose-theme-adapter:1.0.5"
     }
 
     object Compose {
-        const val version = "1.0.4"
+        const val version = "1.0.5"
         const val activity = "androidx.activity:activity-compose:1.3.1"
-        const val constraintlayout =
-            "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha07"
-        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha06"
         const val runtime = "androidx.compose.runtime:runtime:$version"
         const val runtime_livedata = "androidx.compose.runtime:runtime-livedata:$version"
         const val foundation = "androidx.compose.foundation:foundation:$version"
@@ -148,7 +145,7 @@ object Libs {
     }
 
     object Accompanist {
-        private const val version = "0.18.0"
+        private const val version = "0.20.2"
         const val drawablepainter = "com.google.accompanist:accompanist-drawablepainter:$version"
         const val flowlayout = "com.google.accompanist:accompanist-flowlayout:$version"
         const val insets = "com.google.accompanist:accompanist-insets:$version"
@@ -163,7 +160,7 @@ object Libs {
     }
 
     object ComposeExperimental {
-        private const val version = "0.1.5"
+        private const val version = "0.1.6"
         const val material = "com.github.fornewid.compose-experimental:material:$version"
         const val ui = "com.github.fornewid.compose-experimental:ui:$version"
     }
@@ -180,26 +177,26 @@ object Libs {
     }
 
     object Coil {
-        private const val version = "1.3.2"
+        private const val version = "1.4.0"
         const val runtime = "io.coil-kt:coil:$version"
         const val compose = "io.coil-kt:coil-compose:$version"
     }
 
     object Lottie {
-        private const val version = "4.1.0"
+        private const val version = "4.2.1"
         const val compose = "com.airbnb.android:lottie-compose:$version"
     }
 
     object Firebase {
-        const val bom = "com.google.firebase:firebase-bom:28.4.0"
+        const val bom = "com.google.firebase:firebase-bom:29.0.0"
         const val analytics = "com.google.firebase:firebase-analytics-ktx"
         const val config = "com.google.firebase:firebase-config-ktx"
         const val crashlytics = "com.google.firebase:firebase-crashlytics-ktx"
-        const val crashlyticsPlugin = "com.google.firebase:firebase-crashlytics-gradle:2.7.1"
+        const val crashlyticsPlugin = "com.google.firebase:firebase-crashlytics-gradle:2.8.0"
         const val messaging = "com.google.firebase:firebase-messaging-ktx"
         const val dynamicLinks = "com.google.firebase:firebase-dynamic-links-ktx"
         const val perf = "com.google.firebase:firebase-perf-ktx"
         const val perfPlugin = "com.google.firebase:perf-plugin:1.4.0"
-        const val ads = "com.google.android.gms:play-services-ads:20.3.0"
+        const val ads = "com.google.android.gms:play-services-ads:20.5.0"
     }
 }
