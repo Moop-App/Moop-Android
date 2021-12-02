0699799d1565e869a09b9c1c3b55dd8fa06f6f91
Sungyong An (SOUP) <soupyong@gmail.com>
2021-12-02 21:50:48 +0900

    Adopt compose to HomeFilterFragment in ':home' module (#136)

--- feature/home/src/main/java/soup/movie/home/filter/HomeFilterFragment.kt
+++ feature/home/src/main/java/soup/movie/home/filter/HomeFilterFragment.kt
@@ -16,70 +16,29 @@
 package soup.movie.home.filter
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.google.android.material.chip.Chip
+import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
-import soup.movie.home.R
-import soup.movie.home.databinding.HomeFilterFragmentBinding
-import soup.movie.util.inflate
-import soup.movie.util.setOnDebounceClickListener
 
 @AndroidEntryPoint
-class HomeFilterFragment : Fragment(R.layout.home_filter_fragment) {
+class HomeFilterFragment : Fragment() {
 
     private val viewModel: HomeFilterViewModel by viewModels()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        with(HomeFilterFragmentBinding.bind(view)) {
-            theaterItem.cgvView.setOnCheckedChangeListener { filterChip, isChecked ->
-                viewModel.onCgvFilterChanged(isChecked)
-            }
-            theaterItem.lotteView.setOnCheckedChangeListener { filterChip, isChecked ->
-                viewModel.onLotteFilterChanged(isChecked)
-            }
-            theaterItem.megaboxView.setOnCheckedChangeListener { filterChip, isChecked ->
-                viewModel.onMegaboxFilterChanged(isChecked)
-            }
-            viewModel.theaterUiModel.observe(viewLifecycleOwner) {
-                theaterItem.cgvView.isChecked = it.hasCgv
-                theaterItem.lotteView.isChecked = it.hasLotteCinema
-                theaterItem.megaboxView.isChecked = it.hasMegabox
-            }
-
-            ageItem.ageAllView.setOnDebounceClickListener {
-                viewModel.onAgeAllFilterClicked()
-            }
-            ageItem.age12View.setOnDebounceClickListener {
-                viewModel.onAge12FilterClicked()
-            }
-            ageItem.age15View.setOnDebounceClickListener {
-                viewModel.onAge15FilterClicked()
-            }
-            ageItem.age19View.setOnDebounceClickListener {
-                viewModel.onAge19FilterClicked()
-            }
-            viewModel.ageUiModel.observe(viewLifecycleOwner) {
-                ageItem.ageAllView.isSelected = it.hasAll
-                ageItem.age12View.isSelected = it.has12
-                ageItem.age15View.isSelected = it.has15
-                ageItem.age19View.isSelected = it.has19
-            }
-
-            viewModel.genreUiModel.observe(viewLifecycleOwner) {
-                genreItem.genreFilterGroup.run {
-                    removeAllViews()
-                    it.items.forEach {
-                        val genreChip: Chip = inflate(context, R.layout.home_filter_item_genre)
-                        genreChip.text = it.name
-                        genreChip.isChecked = it.isChecked
-                        genreChip.setOnCheckedChangeListener { _, isChecked ->
-                            viewModel.onGenreFilterClick(it.name, isChecked)
-                        }
-                        addView(genreChip)
-                    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MdcTheme {
+                    HomeFilterScreen(viewModel)
                 }
             }
         }
--- feature/home/src/main/java/soup/movie/home/filter/HomeFilterUiModel.kt
+++ feature/home/src/main/java/soup/movie/home/filter/HomeFilterUiModel.kt
@@ -17,14 +17,12 @@ package soup.movie.home.filter
 
 import androidx.annotation.Keep
 
-sealed class HomeFilterUiModel
-
 @Keep
 data class TheaterFilterUiModel(
     val hasCgv: Boolean,
     val hasLotteCinema: Boolean,
     val hasMegabox: Boolean
-) : HomeFilterUiModel()
+)
 
 @Keep
 data class AgeFilterUiModel(
@@ -32,12 +30,7 @@ data class AgeFilterUiModel(
     val has12: Boolean,
     val has15: Boolean,
     val has19: Boolean
-) : HomeFilterUiModel()
-
-@Keep
-data class GenreFilterUiModel(
-    val items: List<GenreFilterItem>
-) : HomeFilterUiModel()
+)
 
 @Keep
 class GenreFilterItem(
