55c24ad3dd1cc2a7fbdc9a5684ad26f754015e2b
Sungyong An (SOUP) <soupyong@gmail.com>
2021-12-06 21:42:01 +0900

    Adopt compose to HomeContentsFragment in ':home' module (#139)

--- feature/home/src/main/java/soup/movie/home/tab/HomeContentsViewModel.kt
+++ feature/home/src/main/java/soup/movie/home/tab/HomeContentsViewModel.kt
@@ -16,12 +16,12 @@
 package soup.movie.home.tab
 
 import androidx.lifecycle.LiveData
-import soup.movie.home.HomeContentsUiModel
+import soup.movie.model.Movie
 
 interface HomeContentsViewModel {
 
     val isLoading: LiveData<Boolean>
     val isError: LiveData<Boolean>
-    val contentsUiModel: LiveData<HomeContentsUiModel>
+    val movies: LiveData<List<Movie>>
     fun refresh()
 }
