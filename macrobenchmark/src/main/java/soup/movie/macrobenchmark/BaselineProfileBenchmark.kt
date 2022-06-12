012c8a814f9c8a02abb54df7b611f387157c22dc
Sungyong An <soupyong@gmail.com>
2022-06-12 22:37:50 +0900

    Create Baseline Profiles (#157)

---
+++ macrobenchmark/src/main/java/soup/movie/macrobenchmark/BaselineProfileBenchmark.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 SOUP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package soup.movie.macrobenchmark
+
+import androidx.benchmark.macro.BaselineProfileMode
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BaselineProfileBenchmark {
+
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun startupNoCompilation() {
+        startup(CompilationMode.None())
+    }
+
+    @Test
+    fun startupBaselineProfile() {
+        startup(
+            CompilationMode.Partial(
+                baselineProfileMode = BaselineProfileMode.Require
+            )
+        )
+    }
+
+    private fun startup(compilationMode: CompilationMode) {
+        benchmarkRule.measureRepeated(
+            packageName = "soup.movie",
+            metrics = listOf(StartupTimingMetric()),
+            iterations = 10,
+            startupMode = StartupMode.COLD,
+            compilationMode = compilationMode
+        ) { // this = MacrobenchmarkScope
+            pressHome()
+            startActivityAndWait()
+        }
+    }
+}
