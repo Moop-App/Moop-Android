169012fef5733cf2f4017a617b6af323b79dd4da
Sungyong An <soupyong@gmail.com>
2022-06-20 20:38:20 +0900

    Compose in RecyclerView (#160)

--- feature/detail/src/main/java/soup/movie/detail/DetailPersonListAdapter.kt
+++ feature/detail/src/main/java/soup/movie/detail/DetailPersonListAdapter.kt
@@ -15,23 +15,33 @@
  */
 package soup.movie.detail
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.view.isGone
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.ext.AlwaysDiffCallback
+import com.google.android.material.composethemeadapter.MdcTheme
 import soup.movie.detail.DetailPersonListAdapter.ViewHolder
-import soup.movie.detail.databinding.DetailItemCastPersonBinding
 import soup.movie.ext.executeWeb
 import soup.movie.util.setOnDebounceClickListener
 
-internal class DetailPersonListAdapter : ListAdapter<PersonUiModel, ViewHolder>(AlwaysDiffCallback()) {
+internal class DetailPersonListAdapter :
+    ListAdapter<PersonUiModel, ViewHolder>(AlwaysDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = DetailItemCastPersonBinding.inflate(layoutInflater, parent, false)
-        return ViewHolder(binding).apply {
+        return ViewHolder(ComposeView(parent.context)).apply {
             itemView.setOnDebounceClickListener {
                 val query = getItem(bindingAdapterPosition).query
                 it.context.executeWeb("https://m.search.naver.com/search.naver?query=$query")
@@ -44,13 +54,46 @@ internal class DetailPersonListAdapter : ListAdapter<PersonUiModel, ViewHolder>(
     }
 
     class ViewHolder(
-        private val binding: DetailItemCastPersonBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
+        private val composeView: ComposeView
+    ) : RecyclerView.ViewHolder(composeView) {
 
         fun bind(item: PersonUiModel) {
-            binding.nameText.text = item.name
-            binding.castText.text = item.cast
-            binding.castText.isGone = item.cast.isEmpty()
+            composeView.setContent {
+                MdcTheme {
+                    CastPerson(item)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CastPerson(item: PersonUiModel) {
+    Card(
+        modifier = Modifier.padding(start = 4.dp, end = 4.dp, bottom = 8.dp),
+        shape = RoundedCornerShape(16.dp),
+        backgroundColor = MaterialTheme.colors.surface,
+        elevation = dimensionResource(R.dimen.detail_card_elevation),
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = item.name,
+                maxLines = 1,
+                color = MaterialTheme.colors.onSurface,
+                style = MaterialTheme.typography.subtitle2,
+                fontWeight = FontWeight.Bold,
+            )
+            if (item.cast.isNotEmpty()) {
+                Text(
+                    text = item.cast,
+                    maxLines = 1,
+                    color = MaterialTheme.colors.onSurface,
+                    style = MaterialTheme.typography.subtitle2,
+                )
+            }
         }
     }
 }
