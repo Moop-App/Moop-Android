cf87062980ef1adf8cda413476cdc3a68c96bf33
fornewid <soupyong@gmail.com>
2022-12-02 00:20:28 +0900

    Setup convention plugins

---
+++ build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
@@ -0,0 +1,24 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
+
+class SpotlessConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.diffplug.spotless")
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+            extensions.configure<SpotlessExtension> {
+                kotlin {
+                    target("**/*.kt")
+                    targetExclude("**/build/**/*.kt")
+                    ktlint(libs.findVersion("ktlint").get().toString())
+                    licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+                }
+            }
+        }
+    }
+}
