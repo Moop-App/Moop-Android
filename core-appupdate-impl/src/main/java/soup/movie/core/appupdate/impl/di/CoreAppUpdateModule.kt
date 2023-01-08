d33d4b187e10cb66ce34149720931dd88f637ab7
fornewid <soupyong@gmail.com>
2023-01-08 19:47:32 +0900

    Split :core-appupdate module to api and impl

--- core-appupdate-impl/src/main/java/soup/movie/core/appupdate/impl/di/CoreAppUpdateModule.kt
+++ core-appupdate-impl/src/main/java/soup/movie/core/appupdate/impl/di/CoreAppUpdateModule.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 SOUP
+ * Copyright 2023 SOUP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package soup.movie.di
+package soup.movie.core.appupdate.impl.di
 
 import android.content.Context
 import dagger.Module
@@ -22,47 +22,21 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
-import soup.movie.ads.AdsConfigImpl
-import soup.movie.analytics.EventAnalyticsImpl
 import soup.movie.common.IoDispatcher
-import soup.movie.core.ads.AdsConfig
-import soup.movie.core.analytics.EventAnalytics
-import soup.movie.core.imageloading.ImageUriProvider
-import soup.movie.feature.work.NotificationBuilder
-import soup.movie.imageloading.ImageUriProviderImpl
-import soup.movie.notification.NotificationBuilderImpl
+import soup.movie.core.appupdate.InAppUpdateManager
+import soup.movie.core.appupdate.impl.InAppUpdateManagerImpl
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class ApplicationModule {
+class CoreAppUpdateModule {
 
     @Singleton
     @Provides
-    fun provideImageUriProvider(
+    fun provideAppUpdateManager(
         @ApplicationContext context: Context,
         @IoDispatcher ioDispatcher: CoroutineDispatcher,
-    ): ImageUriProvider {
-        return ImageUriProviderImpl(context, ioDispatcher)
+    ): InAppUpdateManager {
+        return InAppUpdateManagerImpl(context, ioDispatcher)
     }
-
-    @Singleton
-    @Provides
-    fun provideEventAnalytics(
-        @ApplicationContext context: Context
-    ): EventAnalytics = EventAnalyticsImpl(context)
-
-    @Singleton
-    @Provides
-    fun provideAdsConfig(
-        @ApplicationContext context: Context,
-    ): AdsConfig {
-        return AdsConfigImpl(context)
-    }
-
-    @Singleton
-    @Provides
-    fun provideNotificationBuilder(
-        @ApplicationContext context: Context
-    ): NotificationBuilder = NotificationBuilderImpl(context)
 }
