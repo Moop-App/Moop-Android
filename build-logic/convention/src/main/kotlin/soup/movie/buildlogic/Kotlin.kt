8cfc90d67203a05933d7b5861a55ebb8cbd3ec4b
Sungyong An <sungyong.an@webtoonscorp.com>
2023-12-09 11:38:48 +0900

    Fix R8 issues temporarily

--- build-logic/convention/src/main/kotlin/soup/movie/buildlogic/Kotlin.kt
+++ build-logic/convention/src/main/kotlin/soup/movie/buildlogic/Kotlin.kt
@@ -13,6 +13,9 @@ fun Project.configureKotlin() {
             freeCompilerArgs = freeCompilerArgs + listOf(
                 "-opt-in=kotlin.RequiresOptIn",
                 "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+
+                // https://github.com/Kotlin/kotlinx.serialization/issues/2145#issuecomment-1653091753
+                "-Xstring-concat=inline",
             )
         }
     }
