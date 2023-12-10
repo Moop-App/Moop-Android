1fe0f924b12428c38784f88da524b8dd649da1b3
Sungyong An <soupyong@gmail.com>
2023-12-11 02:13:37 +0900

    Update libraries (#263)
    * Update libraries for Play In-App Updates
    
    * Update libraries for kakao sdk
    
    * Update libraries for Jetpack

--- data/database/impl/src/main/java/soup/movie/data/database/impl/dao/FavoriteMovieDao.kt
+++ data/database/impl/src/main/java/soup/movie/data/database/impl/dao/FavoriteMovieDao.kt
@@ -17,7 +17,7 @@ package soup.movie.data.database.impl.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
