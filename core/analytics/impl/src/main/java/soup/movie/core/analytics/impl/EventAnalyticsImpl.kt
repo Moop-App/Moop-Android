c59e36d1343f0b58486fcd5306c07d777ed262f7
Sungyong An <sungyong.an@webtoonscorp.com>
2023-12-14 01:03:56 +0900

    Run spotlessApply

--- core/analytics/impl/src/main/java/soup/movie/core/analytics/impl/EventAnalyticsImpl.kt
+++ core/analytics/impl/src/main/java/soup/movie/core/analytics/impl/EventAnalyticsImpl.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 SOUP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package soup.movie.core.analytics.impl
 
 import android.app.Activity
@@ -12,8 +27,7 @@ import javax.inject.Singleton
 private typealias Params = Bundle.() -> Unit
 
 @Singleton
-class EventAnalyticsImpl @Inject constructor(
-) : EventAnalytics {
+class EventAnalyticsImpl @Inject constructor() : EventAnalytics {
 
     private val delegate by lazy {
         Firebase.analytics
