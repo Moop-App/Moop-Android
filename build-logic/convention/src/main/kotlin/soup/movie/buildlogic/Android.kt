434edf802d0ce92c607dec952283ff80deb9a84e
Sungyong An <sungyong.an@webtoonscorp.com>
2024-03-02 17:40:39 +0900

    Fix runtime issue for JDK core desugaring

--- build-logic/convention/src/main/kotlin/soup/movie/buildlogic/Android.kt
+++ build-logic/convention/src/main/kotlin/soup/movie/buildlogic/Android.kt
@@ -3,7 +3,10 @@ package soup.movie.buildlogic
 import com.android.build.gradle.BaseExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 fun Project.configureAndroid() {
     android {
@@ -17,6 +20,12 @@ fun Project.configureAndroid() {
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_11
             targetCompatibility = JavaVersion.VERSION_11
+            isCoreLibraryDesugaringEnabled = true
+        }
+
+        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+        dependencies {
+            coreLibraryDesugaring(libs.findLibrary("android.desugar.jdk").get())
         }
     }
 }
