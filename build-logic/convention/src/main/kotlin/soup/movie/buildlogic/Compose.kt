62ff68744bc0261367675a30c0232177f9023c9e
Sungyong An <sungyong.an@webtoonscorp.com>
2024-05-29 02:18:19 +0900

    Migrate to Kotlin 2.0

--- build-logic/convention/src/main/kotlin/soup/movie/buildlogic/Compose.kt
+++ build-logic/convention/src/main/kotlin/soup/movie/buildlogic/Compose.kt
@@ -4,24 +4,27 @@ import com.android.build.gradle.BaseExtension
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 fun Project.configureCompose() {
+    pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
+
     val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
     android {
         buildFeatures.compose = true
-
-        composeOptions {
-            kotlinCompilerExtensionVersion =
-                libs.findVersion("compose-compiler").get().toString()
-        }
 
         dependencies {
             val bom = libs.findLibrary("compose-bom").get()
             implementation(platform(bom))
             androidTestImplementation(platform(bom))
         }
+    }
+
+    extensions.configure<ComposeCompilerGradlePluginExtension> {
+        enableStrongSkippingMode = false
     }
 }
 
