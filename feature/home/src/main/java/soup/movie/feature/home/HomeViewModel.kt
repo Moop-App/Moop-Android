8ce3b25b147c8db6e68a56807e403ad757ae637a
Sungyong An <sungyong.an@webtoonscorp.com>
2024-10-27 23:19:53 +0900

    Fix spotless

--- feature/home/src/main/java/soup/movie/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/soup/movie/feature/home/HomeViewModel.kt
@@ -24,8 +24,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(
-) : ViewModel() {
+class HomeViewModel @Inject constructor() : ViewModel() {
 
     private val _selectedMainTab = MutableStateFlow(MainTabUiModel.Home)
     val selectedMainTab: StateFlow<MainTabUiModel> = _selectedMainTab
